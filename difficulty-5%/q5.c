b15f2813bec72dd8a81824d408f53a82bdeef3bb
Ismail Musharaf <ismailmushraf@Ismails-MacBook-Pro.local>
2023-12-09 08:59:57 +0530

    Optimized solution for Q5: Smallest Multiple

--- difficulty-5%/q5.c
+++ difficulty-5%/q5.c
@@ -1,18 +1,41 @@
 #include<stdio.h>
 
-int main()
+long long int smallestMultiple(int);
+long long int lcm(long long int, long long int);
+long long int gcd(long long int, long long int);
+
+
+int main() 
 {
-  for (unsigned long long int i = 20; i < 900000000; i+=20)
+  int n;
+  printf("Enter a Number: ");
+  scanf("%d", &n);
+  long long int result = smallestMultiple(n); 
+  printf("The Smallest number divisible by 1 to %d is %lld\n", n, result);
+}
+
+long long int smallestMultiple(int num)
+{
+  long long int result = 1;
+  for (int i = 2; i <= num; i++)
   {
-    int divisible_by_1to20 = 1;
-    for (int j = 1; j <= 20; j++)
-    {
-      if (i % j != 0)
-      {
-        divisible_by_1to20 = 0;
-        break;
-      }
-    }
-    if (divisible_by_1to20) printf("%lld\n", i);
+    result = lcm(result, i);
   }
+  return result;
+}
+
+long long int lcm(long long a, long long b)
+{
+  return (a * b)/gcd(a, b);
+}
+
+long long int gcd(long long int a, long long int b)
+{
+  while (b!=0)
+  {
+    long long temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
 }
